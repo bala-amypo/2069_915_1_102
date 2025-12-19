01a1d8894f995d5c664e383601e3f675795dad2d
bala-amypo <bala@amypo.in>
2025-12-19 06:19:17 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,5 +1,10 @@
+// src/main/java/com/example/demo/dto/AuthResponse.java
 package com.example.demo.dto;
 
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class AuthResponse {
-    
+    private String token;
+    private String role;
 }
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,5 +1,11 @@
+// src/main/java/com/example/demo/dto/RegisterRequest.java
 package com.example.demo.dto;
 
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class RegisterRequest {
-    
+    private String name;
+    private String email;
+    private String password;
 }
