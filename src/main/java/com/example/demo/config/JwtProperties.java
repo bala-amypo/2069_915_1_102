5e4d8ca0303b17f9e84436405b56c3a61ab98a65
bala-amypo <bala@amypo.in>
2025-12-20 12:25:51 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtProperties.java
+++ src/main/java/com/example/demo/config/JwtProperties.java
@@ -1,12 +1,5 @@
 package com.example.demo.config;
 
 public class JwtProperties {
-    private String secret;
-    private long expirationMs;
-
-    public String getSecret() { return secret; }
-    public void setSecret(String secret) { this.secret = secret; }
-
-    public long getExpirationMs() { return expirationMs; }
-    public void setExpirationMs(long expirationMs) { this.expirationMs = expirationMs; }
+    
 }
