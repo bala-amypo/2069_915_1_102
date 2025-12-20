4b3e4c7648af7b89ed06490cfc21662219d6e8a7
bala-amypo <bala@amypo.in>
2025-12-20 12:01:43 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -7,31 +7,4 @@ public class JwtTokenProvider {
         // Accept config, do nothing
     }
 
-    public String createToken(Long userId, String email, String role) {
-        return "dummy-token";
-    }
-
-    public boolean validateToken(String token) {
-        return true;
-    }
-
-    public DummyClaims getClaims(String token) {
-        return new DummyClaims();
-    }
-
-    public static class DummyClaims {
-        public Body getBody() {
-            return new Body();
-        }
-
-        public static class Body {
-            public Integer get(String key, Class<Integer> type) {
-                return 11; // dummy userId
-            }
-
-            public String get(String key) {
-                return "c@d.com"; // dummy email
-            }
-        }
-    }
 }
