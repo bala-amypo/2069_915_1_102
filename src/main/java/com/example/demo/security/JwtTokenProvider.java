40fe8d1a70aad93fe78101cd730a8afa6483a22b
bala-amypo <bala@amypo.in>
2025-12-21 13:36:51 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,4 +1,5 @@
 package com.example.demo.security;
+import com.example.demo.config.JwtProperties;
 public class JwtTokenProvider {
     public JwtTokenProvider(JwtProperties props) {
         // Accept config, do nothing
