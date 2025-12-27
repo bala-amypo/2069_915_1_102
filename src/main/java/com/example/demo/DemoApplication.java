6a1b49a0c334a2586909ab833c4bc86bb4ff9981
bala-amypo <bala@amypo.in>
2025-12-27 03:24:59 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,5 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        String raw = "string123456";
-        String hash = "$2a$10$7GSNH5QuptSeEUxQus6o1.eR/ICX2ABKSDRafX1jdlkkgcSdJ4lCG";
-
-        boolean matches = new BCryptPasswordEncoder().matches(raw, hash);
-        System.out.println("Matches: " + matches);
     }
 }
