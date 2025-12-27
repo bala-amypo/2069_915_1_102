d3eb2fbc940463dc0c9a9f3f3fcf0a50651c1619
bala-amypo <bala@amypo.in>
2025-12-27 18:07:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertSchedule.java
+++ src/main/java/com/example/demo/entity/AlertSchedule.java
@@ -1,8 +1,15 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "alert_schedules")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AlertSchedule {
 
     @Id
@@ -12,25 +19,6 @@ public class AlertSchedule {
     private int daysBeforeExpiry;
 
     @ManyToOne
+    @JoinColumn(name = "warranty_id")
     private Warranty warranty;
-
-    public Long getId() {
-        return id;
-    }
-
-    public int getDaysBeforeExpiry() {
-        return daysBeforeExpiry;
-    }
-
-    public void setDaysBeforeExpiry(int daysBeforeExpiry) {
-        this.daysBeforeExpiry = daysBeforeExpiry;
-    }
-
-    public Warranty getWarranty() {
-        return warranty;
-    }
-
-    public void setWarranty(Warranty warranty) {
-        this.warranty = warranty;
-    }
 }
