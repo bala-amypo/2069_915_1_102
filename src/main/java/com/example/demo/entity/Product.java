d127550df34712f6a151a28dcc2433170a992477
bala-amypo <bala@amypo.in>
2025-12-30 05:45:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -24,12 +24,5 @@ public class Product {
     private String modelNumber;
     private String category;
 
-    // REQUIRED
-    public Product(Long id, String name, String brand, String modelNumber, String category) {
-        this.id = id;
-        this.name = name;
-        this.brand = brand;
-        this.modelNumber = modelNumber;
-        this.category = category;
-    }
+    
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-
+@AllArgsConstructor
 @Builder   // ✅ REQUIRED FOR builder()
 public class User {
 
@@ -20,12 +20,5 @@ public class User {
     private String password;
     private String role;
 
-    // ✅ REQUIRED: used directly in tests
-    public User(Long id, String name, String email, String password, String role) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
+   
 }
